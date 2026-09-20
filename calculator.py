f520e31e7eedc5a5c545b5fd9b9fa13bb313145c
kayleeCromwell <128537385+kayleeCromwell@users.noreply.github.com>
2026-09-19 21:02:39 -0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    add = a+b
+    total = add
+    return total
+def subtract(a,b):
+    sub = a-b
+    total = sub
+    return total
+def divide(a,b):
+    div = a/b
+    total = div
+    return total
